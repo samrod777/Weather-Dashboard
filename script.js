f6e382cfd040b3fe7ccf91546ffdb43b331c387d
Sam Rodriguez <rodriguez_414@yahoo.com>
2020-10-09 23:32:33 -0700

    current weather working

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+moment().format('L');
+
+function searchCity(cityname) {
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=f1ecd9ec5526a01556f7eaa323c1c11f";
+    var queryURLforcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=f1ecd9ec5526a01556f7eaa323c1c11f";
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+        console.log(queryURL);
+        //empty divs and ids that we need to dump content into.....
+        $("#current").empty();
+       var mainDate = moment().format('ddd l LT');
+ 
+
+        //create HTML for city information......
+        var cityNameEl = $("<h2>").text(response.name);
+        var displayMainDate = cityNameEl.append(" " + "-- " + mainDate);
+        var tempEL = $("<p>").text("Tempraturer: " + Math.trunc(response.main.temp) + " \xB0F");
+        var humEl = $("<p>").text("Humidity: " + response.main.humidity + " %");
+        var windEl = $("<p>").text("Wind Speed: " + Math.trunc(response.wind.speed) + " mph");
+        var currentweather = response.weather[0].main;
+
+        if (currentweather === "Rain") {
+            var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/09d.png");
+            currentIcon.attr("style", "height: 60px; width: 60px");
+        } else if (currentweather === "Clouds") {
+            var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/03d.png");
+            currentIcon.attr("style", "height: 60px; width: 60px");
+        } else if (currentweather === "Clear") {
+            var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/01d.png");
+            currentIcon.attr("style", "height: 60px; width: 60px");
+        } else if (currentweather === "Drizzle") {
+            var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/10d.png");
+            currentIcon.attr("style", "height: 60px; width: 60px");
+        } else if (currentweather === "Snow") {
+            var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/13d.png");
+            currentIcon.attr("style", "height: 60px; width: 60px");
+        }
+        //create HTML div to append new elements to render on page....
+        var newDiv = $('<div>');
+
+        newDiv.append(displayMainDate, currentIcon, tempEL, humEl, windEl);
+
+        $("#current").html(newDiv);
+ 
+});
